9f60c7f60245280b213365735fe59a5a96d5881f
YRRY <18755246110@163.com>
2026-07-12 07:13:58 +0800

    firmware: add JY62 gyro diagnostic display

--- firmware/WHEELTEC_C07A_CAR_JY62_GapBridge/Control/show.c
+++ firmware/WHEELTEC_C07A_CAR_JY62_GapBridge/Control/show.c
@@ -3,6 +3,52 @@ Brand: WHEELTEC
 ***********************************************/
 #include "show.h"
 #include "control.h"
+#include "jy62.h"
+
+static void OLED_ShowSignedNumber(uint8_t x, uint8_t y, int value, uint8_t len)
+{
+    OLED_ShowString(x, y, (value < 0) ? "-" : "+");
+    OLED_ShowNumber(x + 8U, y, myabs(value), len, 12);
+}
+
+static void oled_show_jy62_diag(void)
+{
+    JY62_Data data;
+
+    memset(OLED_GRAM, 0, 128 * 8 * sizeof(u8));
+    JY62_GetData(&data);
+
+    OLED_ShowString(0, 0, "JY");
+    OLED_ShowString(18, 0, data.online ? "ON " : "OFF");
+    OLED_ShowString(48, 0, "VF");
+    OLED_ShowNumber(66, 0, data.valid_frames % 10000U, 4, 12);
+    OLED_ShowString(0, 10, "ER");
+    OLED_ShowNumber(18, 10, data.checksum_errors % 10000U, 4, 12);
+
+    OLED_ShowString(0, 20, "R");
+    OLED_ShowSignedNumber(10, 20, (int)data.roll_deg, 3);
+    OLED_ShowString(46, 20, "P");
+    OLED_ShowSignedNumber(56, 20, (int)data.pitch_deg, 3);
+    OLED_ShowString(92, 20, "Y");
+    OLED_ShowSignedNumber(102, 20, (int)data.yaw_deg, 3);
+
+    OLED_ShowString(0, 30, "GX");
+    OLED_ShowSignedNumber(14, 30, (int)data.wx_dps, 3);
+    OLED_ShowString(50, 30, "GY");
+    OLED_ShowSignedNumber(64, 30, (int)data.wy_dps, 3);
+
+    OLED_ShowString(0, 40, "GZ");
+    OLED_ShowSignedNumber(14, 40, (int)data.wz_dps, 3);
+    OLED_ShowString(54, 40, "IY");
+    OLED_ShowSignedNumber(70, 40, (int)data.gyro_yaw_int_deg, 3);
+
+    OLED_ShowString(0, 50, "IX");
+    OLED_ShowSignedNumber(14, 50, (int)data.gyro_roll_int_deg, 3);
+    OLED_ShowString(54, 50, "IP");
+    OLED_ShowSignedNumber(70, 50, (int)data.gyro_pitch_int_deg, 3);
+
+    OLED_Refresh_Gram();
+}
 
 void oled_show(void)
 {
@@ -11,6 +57,11 @@ void oled_show(void)
     int yaw_show;
     int err_show;
     char point_str[2];
+
+    if (Flag_Stop) {
+        oled_show_jy62_diag();
+        return;
+    }
 
     memset(OLED_GRAM, 0, 128 * 8 * sizeof(u8));
     point_str[0] = (char) Gray_LastPointChar;
